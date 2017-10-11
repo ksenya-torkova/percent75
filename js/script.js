6e77e0a9f4a561d0833d7f3b21ba2481ac98be86
ksenya-torkova <ksenya.torkova@gmail.com>
2017-10-11 18:15:23 +0300

    faq.html

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@ var siteHeader = document.querySelector('.site-header');
 var siteHeaderButton = siteHeader.querySelector('.site-header__button');
 var siteHeaderWrapper = siteHeader.querySelector('.site-header__wrapper');
 
-siteHeaderWrapper.classList.remove('site-header__wrapper--no-js');
-siteHeaderWrapper.classList.add('site-header__wrapper--hidden');
-siteHeader.classList.add('site-header--close-menu');
-siteHeaderButton.classList.remove('site-header__button--close');
-
 siteHeaderButton.addEventListener('click', function() {
   siteHeaderButton.classList.toggle('site-header__button--close');
 
