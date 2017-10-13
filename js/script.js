9e765bb34cae8620378c7d5cabefae2a69671a17
ksenya-torkova <ksenya.torkova@gmail.com>
2017-10-13 14:28:27 +0300

    navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var siteHeader = document.querySelector('.site-header');
 var siteHeaderButton = siteHeader.querySelector('.site-header__button');
 var siteHeaderWrapper = siteHeader.querySelector('.site-header__wrapper');
+var flexWrapper = document.querySelector('.flex-wrapper');
 
 siteHeaderButton.addEventListener('click', function() {
   siteHeaderButton.classList.toggle('site-header__button--close');
@@ -11,5 +12,11 @@ siteHeaderButton.addEventListener('click', function() {
   } else {
     siteHeader.classList.add('site-header--close-menu');
     siteHeaderWrapper.classList.add('site-header__wrapper--hidden');
+  }
+  
+  if(siteHeader.classList.contains('site-header--close-menu')) {
+    flexWrapper.classList.remove('flex-wrapper--padding');
+  } else {
+    flexWrapper.classList.add('flex-wrapper--padding');
   }
 });
