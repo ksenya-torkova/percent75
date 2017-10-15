29676b4852f9ffd2cbf879320ef905c634a69325
ksenya-torkova <ksenya.torkova@gmail.com>
2017-10-15 15:21:08 +0300

    calc script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// navbar script
 var siteHeader = document.querySelector('.site-header');
 var siteHeaderButton = siteHeader.querySelector('.site-header__button');
 var siteHeaderWrapper = siteHeader.querySelector('.site-header__wrapper');
@@ -20,3 +21,59 @@ siteHeaderButton.addEventListener('click', function() {
     flexWrapper.classList.add('flex-wrapper--padding');
   }
 });
+
+// calc script
+var calcForm = document.querySelector('.calc__form');
+var tariff = calcForm.querySelector('#tariff');
+var invest = calcForm.querySelector('#invest');
+var term = calcForm.querySelector('#term');
+var income = calcForm.querySelector('#income');
+var profit = calcForm.querySelector('#profit');
+var calcButton = calcForm.querySelector('#calc-button');
+
+var selectTariff = function () {
+  var tariffValue = null;
+
+  switch (tariff.value) {
+    case 'start':
+      tariffValue = 0.75;
+      break;
+    case 'medium':
+      tariffValue = 1;
+      break;
+    case 'premium':
+      tariffValue = 1.25;
+      break;
+  }
+
+  return tariffValue;
+}
+
+tariff.addEventListener('change', function () {
+  if (selectTariff() === 0.75) {
+    term.value = 25;
+    invest.min = 25;
+  } else if (selectTariff() === 1) {
+    term.value = 50;
+    invest.min = 250;
+  } else if (selectTariff() === 1.25) {
+    term.value = 75;
+    invest.min = 750;
+  }
+});
+
+calcButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  
+  if (parseInt(invest.value, 10) >= parseInt(invest.min, 10)) {
+    invest.classList.remove('form__input--error');
+    invest.style.background='white';
+    income.value = parseInt(invest.value, 10) + (parseInt(invest.value, 10) / 100) * selectTariff() * parseInt(term.value, 10);
+    profit.value = selectTariff() * parseInt(term.value, 10) + '%';
+  } else if (invest.validity.valueMissing || invest.validity.rangeUnderflow) {
+    invest.classList.add('form__input--error');
+  }
+});
+
+// tabs script
+
